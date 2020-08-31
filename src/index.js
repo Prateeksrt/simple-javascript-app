275c122a76ddd8acef38e8e0aa9cbc3aa2471365
Prateek Srivastava <Prateek.Srivastava@agoda.com>
2020-08-31 15:35:03 +0700

    Added the insert item to list

--- src/index.js
+++ src/index.js
@@ -2,13 +2,6 @@ function getById(id) {
     return document.getElementById(id);
 }
 
-function createDeleteButton(id) {
-    const deleteButton = document.createElement("Button")
-    deleteButton.innerText = "x";
-    deleteButton.onclick = () => deleteItem(id);
-    return deleteButton;
-}
-
 function createListItem(value) {
     const newListItem = document.createElement("span");
     newListItem.innerText = value;
@@ -17,12 +10,10 @@ function createListItem(value) {
 
 function createNewEntry(value, id) {
     const newListItem = createListItem(value, id);
-    const deleteButton = createDeleteButton(id);
     
     const div = document.createElement("li");
     div.id = id;
     div.appendChild(newListItem);
-    div.appendChild(deleteButton);
         
     return div;
 }
@@ -41,7 +32,4 @@ function addItem() {
     clearValueOf(getById("item-value"));
 }
 
-function deleteItem(id) {
-    document.getElementById(id).remove();
-}
 console.log("Index.js, Initialized successfully!");
